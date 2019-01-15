7c9ae87a53b4d60eef0fbda5c9a350adf3edb1a4
Josh <trismix@protonmail.com>
2019-01-15 16:55:03 -0500

    fixed subs

---
+++ jsonresume-theme-onepage-nderkach/index.js
@@ -0,0 +1,74 @@
+var fs = require("fs");
+var Handlebars = require("handlebars");
+const moment = require('moment');
+
+COURSES_COLUMNS = 3;
+
+PREPEND_SUMMARY_CATEGORIES = [
+  "work",
+  "volunteer",
+  "awards",
+  "publications"
+];
+
+function validateArray(arr) {
+  return arr !== undefined && arr !== null && arr instanceof Array && arr.length > 0;
+}
+
+const dateHelpers = {
+  MY: date => moment(date.toString(), ['YYYY-MM-DD']).format('MMMM YYYY'),
+  Y: date => moment(date.toString(), ['YYYY-MM-DD']).format('YYYY'),
+  DMY: date => moment(date.toString(), ['YYYY-MM-DD']).format('D MMMM YYYY')
+};
+
+Handlebars.registerHelper('MY', dateHelpers.MY);
+Handlebars.registerHelper('Y', dateHelpers.Y);
+Handlebars.registerHelper('DMY', dateHelpers.DMY);
+
+function render(resume) {
+  // Split courses into 3 columns
+  if (validateArray(resume.education)) {
+    resume.education.forEach(function(block) {
+      if (validateArray(block.courses)) {
+        splitCourses = [];
+        columnIndex = 0;
+        for (var i = 0; i < COURSES_COLUMNS; i++) {
+          splitCourses.push([]);
+        }
+        block.courses.forEach(function(course) {
+          splitCourses[columnIndex].push(course);
+          columnIndex++;
+          if (columnIndex >= COURSES_COLUMNS) {
+            columnIndex = 0;
+          }
+        });
+        block.courses = splitCourses;
+      }
+    });
+  }
+
+  PREPEND_SUMMARY_CATEGORIES.forEach(function(category) {
+    if (resume[category] !== undefined) {
+      resume[category].forEach(function(block) {
+        if (block.highlights === undefined) {
+          block.highlights = [];
+        }
+        if (block.summary) {
+          block.highlights.unshift(block.summary);
+          delete block.summary;
+        }
+      });
+    }
+  });
+
+	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+	return Handlebars.compile(tpl)({
+		css: css,
+		resume: resume
+	});
+}
+
+module.exports = {
+	render: render
+};
